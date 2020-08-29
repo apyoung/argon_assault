f290f2e348c0c322030e3907d44aa7a025c3a4e5
Andrew Young <apyoung88@gmail.com>
2020-08-29 11:55:07 -0400

    Create ScoreBoard class

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 12;
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        updateScore();
+    }
+
+    private void updateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit()
+    {
+        score += scorePerHit;
+        updateScore();
+    }
+}
